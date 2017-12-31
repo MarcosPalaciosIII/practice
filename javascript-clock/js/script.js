10f5c3088102174cc83543efdbf5f93c21d688ca
Shincero <mp2772@gmail.com>
2017-12-31 11:27:12 -0500

    created current timer clock with javascript

---
+++ javascript-clock/js/script.js
@@ -0,0 +1,31 @@
+function timer() {
+  var currentDay = new Date();
+  var hour = currentDay.getHours();
+  var minutes = currentDay.getMinutes();
+  var seconds = currentDay.getSeconds();
+
+  minutes = currentTime(minutes);
+  seconds = currentTime(seconds);
+
+  function currentTime(i) {
+    if (i < 10) {
+      i = "0" + i;
+    }
+    return i;
+  }
+
+  document.querySelector(".currentTime").innerHTML = hour + ":" + minutes + ":" + seconds;
+
+  setTimeout(function () {
+    timer();
+  },
+   1000
+ );
+
+}
+
+
+
+
+
+console.log(timer());
