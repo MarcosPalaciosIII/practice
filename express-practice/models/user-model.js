827f579d40f985a5f48c3ea13391ef5eb8d36dfc
Shincero <mp2772@gmail.com>
2018-03-10 11:33:15 -0500

    made changes to stylesheet and set up router for user signup

--- express-practice/models/user-model.js
+++ express-practice/models/user-model.js
@@ -3,7 +3,25 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const userSchema = new Schema(
-
+  {
+    userName: {
+      type: String,
+      required: [true, "Please select a username"]
+    },
+    email: {
+      type: String,
+      required: [true, "Please add a valid email address"]
+    },
+    encryptedPassword: {
+      type: String,
+      required: [true, "Please create a password"],
+      match: [/.+@.+/, "emails must contain an @ sign"]
+    }
+  },
+  {
+    // add "createdAt" and "updatedAt" fields
+    timestamps: true
+  }
 );
 
 
--- express-practice/routes/user-router.js
+++ express-practice/routes/user-router.js
@@ -7,6 +7,19 @@ const UserModel = require('../models/user-model');
 const router = express.Router();
 
 
+router.get('/signup', (req, res, next) => {
+  if(req.user){
+
+    // redirect user to homepage if already logged in
+    res.redirect("/");
+
+    // early return to stop the function since there's an error
+    //(prevents the rest of the code form running
+    return;
+  }
+  res.render("user-views/signup-page");
+});
+
 
 
 
