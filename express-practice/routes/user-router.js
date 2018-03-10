bece9c4587d2d85cb108092a0fae552f7bcbc175
Shincero <mp2772@gmail.com>
2018-03-10 10:47:02 -0500

    updated express practice model and router to set up signup page

---
+++ express-practice/routes/user-router.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const bcryst = require('bcrypt');
+const passport = require('passport');
+
+const UserModel = require('../models/user-model');
+
+const router = express.Router();
+
+
+
+
+
+module.exports = router;
